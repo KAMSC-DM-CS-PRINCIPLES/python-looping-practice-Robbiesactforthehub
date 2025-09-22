568276c227a73bc6a33a9090447d3f52c83e4797
Robbiesactforthehub <03robert.strongmorse@kamsc.org>
2025-09-22 13:06:24 -0400

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,12 +1,5 @@
 def reverse_string(text):
-    # TODO create reverse string function
-    # HINT: Use for i in range(len(text) - 1, -1, -1): to go backwards
-    # HINT: Build reversed_text character by character: reversed_text += text[i]
-    # HINT: Start with reversed_text = ""
-    # HINT: Return the reversed_text
-    # HINT: NO SLICING ALLOWED - must use loops only
-    pass
-
-if __name__ == "__main__":
-    # create reverse string below this
-    pass
+    esrever=[]
+    for i in range(len(text)):
+        esrever.append(text[len(text)-i-1])
+    return esrever
